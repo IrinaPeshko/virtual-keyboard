2251dfa2340accdc11cf86b319f14418d09cc78a
IrinaPeshko <privet-kira1999@mail.ru>
2023-04-27 23:11:35 +0300

    feat: create HTML with js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,51 @@
 import { buttons, functionButtons } from "./buttons.js";
-console.log(buttons)
-console.log(functionButtons);
+
+const body = document.querySelector(".body");
+const container = document.createElement("div");
+container.className = "container";
+const content = document.createElement("div");
+content.className = "content";
+const header = document.createElement("h1");
+header.className = "header";
+header.innerText = "Virtual Keyboard";
+const textArea = document.createElement("textarea");
+textArea.className = "textarea";
+const keyboardWrapper = document.createElement("div");
+keyboardWrapper.className = "keyboard";
+const button = document.createElement("button");
+button.className = "keyboard__button";
+const pToWindows = document.createElement("p");
+pToWindows.className = "prescription";
+pToWindows.innerText = "Клавиатура была создана в операционной системе Windows";
+const pToLanguage = document.createElement("p");
+pToLanguage.className = "prescription";
+pToLanguage.innerText = "Для переключения языка комбинация: левые Ctr + Alt";
+
+function createKeyboard(){
+  body.prepend(container)
+  container.append(content)
+  container.append(pToWindows)
+  container.append(pToLanguage);
+  content.append(header)
+  content.append(textArea)
+  content.append(keyboardWrapper)
+}
+
+function createButtons(){
+  const buttonsArray = Object.keys(buttons);
+  for(let key in buttons){
+    const button = document.createElement("button");
+    button.className = "keyboard__button";
+    button.innerText = buttons[key].en.lower;
+    keyboardWrapper.append(button)
+    if(functionButtons.includes(key))
+    {
+      button.classList.add("keyboard__button_function");
+      button.classList.add(`keyboard__${key}`); 
+    }
+  }
+  console.log(buttonsArray);
+}
+
+createButtons()
+createKeyboard()
